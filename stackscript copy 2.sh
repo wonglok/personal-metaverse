cb99a3ed9a77dfcc35582c6bf8ac02ee09127573
wonglok <yellowhappy831@gmail.com>
2022-02-22 11:16:48 +0800

    save

--- stackscript copy 2.sh
+++ stackscript copy 2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# <UDF name="USER_PASS" Label="NodeJS Runtime Password" />
-
+# <UDF name="USER_PASS" Label="NodeJS Linux User Password" />
 
 # All done
 echo "Starting Setup!"
@@ -20,17 +19,12 @@ exec 2>&1
 sudo apt-get -y -o Acquire::ForceIPv4=true -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Acquire::ForceIPv4=true -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
-# add-apt-repository ppa:certbot/certbot
-# apt-get install certbot
-
 echo "Starting setup mongodb!"
 
 apt-get -y install mongodb
 
-# Project specific vars
+
 NODE_USER="nodejs"
-GITHUB_USER="wonglok"
-GITHUB_REPO="personal-metaverse"
 
 # SSH
 echo 'AddressFamily inet' | sudo tee -a /etc/ssh/sshd_config
@@ -39,6 +33,19 @@ sed -re 's/^(PasswordAuthentication)([[:space:]]+)yes/\1\2no/' -i.'' /etc/ssh/ss
 sed -re 's/^(UsePAM)([[:space:]]+)yes/\1\2no/' -i.'' /etc/ssh/sshd_config
 sed -re 's/^(PermitRootLogin)([[:space:]]+)yes/\1\2no/' -i.'' /etc/ssh/sshd_config
 sudo systemctl restart sshd
+
+echo "Starting setup user!"
+
+# Add user
+cp /root/.bashrc /etc/skel/.bashrc
+adduser --disabled-password --gecos "" --shell /bin/bash $NODE_USER
+usermod -aG sudo $NODE_USER
+echo "$NODE_USER:$USER_PASS" | sudo chpasswd
+mkdir -p /home/$NODE_USER/.ssh
+cat /root/.ssh/authorized_keys >> /home/$NODE_USER/.ssh/authorized_keys
+chown -R "$NODE_USER":"$NODE_USER" /home/$NODE_USER/.ssh
+
+login $NODE_USER
 
 echo "Starting setup nvm!"
 
@@ -52,21 +59,14 @@ nvm install 16
 nvm use 16
 npm install -g pm2
 
-echo "Starting setup nodejs!"
+echo "Adding App Files"
 
-# Add user
-cp /root/.bashrc /etc/skel/.bashrc
-adduser --disabled-password --gecos "" --shell /bin/bash $NODE_USER
-usermod -aG sudo $NODE_USER
-echo "$NODE_USER:$USER_PASS" | sudo chpasswd
-mkdir -p /home/$NODE_USER/.ssh
-cat /root/.ssh/authorized_keys >> /home/$NODE_USER/.ssh/authorized_keys
-chown -R "$NODE_USER":"$NODE_USER" /home/$NODE_USER/.ssh
-
-echo "installing nodejs setup!"
+# Project specific vars
+GITHUB_USER="wonglok"
+GITHUB_REPO="personal-metaverse"
 
 # Install app
-APP_DIR="/root/$GITHUB_REPO"
+APP_DIR="/home/$NODE_USER/$GITHUB_REPO"
 curl -L https://github.com/$GITHUB_USER/$GITHUB_REPO/tarball/master | tar zx
 mkdir -p $APP_DIR
 mv -T $GITHUB_USER-$GITHUB_REPO-* $APP_DIR
